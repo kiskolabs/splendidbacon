f24397427768a5e53ae169f7bd2dc34d9db278f2
Joao Carlos <mail@joao-carlos.com>
2010-10-16 06:29:11 +0300

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source "http://rubygems.org"
+
+gem "rails", "3.0.1"
+gem "unicorn"
+gem "haml"
+gem "devise"
+gem "simple_form"
+gem "RedCloth"
+
+group :development do
+  gem "sqlite3-ruby", :require => "sqlite3"
+end
+
+group :production do
+  gem "mysql2"
+end
+
+group :test do
+  gem "rspec-rails"
+  gem "cucumber-rails"
+  gem "factory_girl_rails"
+  gem "capybara"
+  gem "launchy"
+  gem "autotest"
+  gem "autotest-growl"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SplendidBacon::Application.config.secret_token = 'cb44cc3b67a9c21b313be96b03afacee122ffe0e8b690244c8bace9ccfcac2c450af21a727ed2fa21270de58e3faf40c1cc297464c9205cb09f772aeb5fc5215'
