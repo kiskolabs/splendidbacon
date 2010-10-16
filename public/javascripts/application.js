bc039902feb7f5e30206426e65edd3b019da4050
Joao Carlos <mail@joao-carlos.com>
2010-10-16 22:47:11 +0300

    Timeline, timeline, timeline.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,18 @@
-function ClearForm() {
-  document.new_invitation.invitation_email.value= "";
-}
+jQuery(function() {
+
+  function ClearForm() {
+    document.new_invitation.invitation_email.value= "";
+  }
+
+  var scroll = 448;
+
+  $("a[href='#next']").click(function() {
+    $("#timeline .project, .month, #today_bar").animate({ left: "-=" + scroll + "px" });
+    return false;
+  });
+
+  $("a[href='#prev']").click(function() {
+    $("#timeline .project, .month, #today_bar").animate({ left: "+=" + scroll + "px" });
+    return false;
+  });
+});
