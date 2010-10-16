ad3ebfb387bb57f6b1ccc86df0a18d91d673556a
Joao Carlos <mail@joao-carlos.com>
2010-10-16 09:35:19 +0300

    Refactored organizations controller and index view.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,14 @@
 class OrganizationsController < ApplicationController
+  respond_to :html
+
   before_filter :authenticate_user!
-  before_filter :check_permissions, :only => [:edit, :update, :destroy]
+  before_filter :current_organization, :only => [:show, :edit, :update, :destroy]
   
   def index
     @organizations = current_user.organizations
+  end
+
+  def show
   end
   
   def new
@@ -14,22 +19,19 @@ class OrganizationsController < ApplicationController
     @organization = Organization.new(params[:organization])
     if @organization.save
       Membership.create!(:user_id => current_user, :organization_id => @organization)
-      redirect_to(@organization, :notice => 'Organization was successfully created.')
-    else
-      render :action => "new"
+      flash[:notice] = 'Organization was successfully created.'
     end
+    respond_with @organization
   end
   
   def edit
-
   end
   
   def update
     if @organization.update_attributes(params[:organization])
-      redirect_to(@organization, :notice => 'Organization was successfully updated.')
-    else
-      render :action => "edit"
+      flash[:notice] = 'Organization was successfully updated.'
     end
+    respond_with @organization
   end
   
   def destroy
@@ -38,10 +40,9 @@ class OrganizationsController < ApplicationController
   end
   
   private
-  
-  def check_create_permissions
-    @organization = Organization.find(params[:id])
-    redirect_to(root_path, :notice => "You're not allowed to touch this organization.") unless current_user.organizations.include?(@organization)
+
+  def current_organization
+    @organization = current_user.organizations.readonly(false).find(params[:id])
   end
   
 end
