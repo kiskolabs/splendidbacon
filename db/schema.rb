14dcfcd5be9740535e8d4b5fce61de652072fa04
Joao Carlos <mail@joao-carlos.com>
2010-10-16 19:34:24 +0300

    DB schema updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20101016073008) do
 
   create_table "invitations", :force => true do |t|
