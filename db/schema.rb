2d5cccad4037282f704e475713c952b8f7d7f82e
Joao Carlos <mail@joao-carlos.com>
2010-10-16 12:03:53 +0300

    Latest DB schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016062711) do
+ActiveRecord::Schema.define(:version => 20101016065627) do
 
   create_table "memberships", :force => true do |t|
     t.integer  "user_id"
