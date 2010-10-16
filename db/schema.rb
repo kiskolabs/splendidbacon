2e8241f9b83fa3bfadc31cb13437de118d467981
Matias Korhonen <matias@kiskolabs.com>
2010-10-16 09:21:55 +0300

    Org. - Proj. relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016050131) do
+ActiveRecord::Schema.define(:version => 20101016061500) do
+
+  create_table "memberships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "organizations", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "participations", :force => true do |t|
     t.integer "user_id"
@@ -23,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20101016050131) do
     t.boolean "active"
     t.string  "name"
     t.string  "token"
+    t.integer "organization_id"
   end
 
   create_table "users", :force => true do |t|
