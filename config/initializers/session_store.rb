6d48bd0edd767e050fc36e4cd7f9df7f504f53c8
Matias Korhonen <matias@kiskolabs.com>
2011-01-04 17:54:06 +0200

    Devise explodes with secure cookies.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SplendidBacon::Application.config.session_store :cookie_store, :key => '_splendid_bacon_secure_session', :secure => Rails.env.production?
+SplendidBacon::Application.config.session_store :cookie_store, :key => '_splendid_bacon_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
