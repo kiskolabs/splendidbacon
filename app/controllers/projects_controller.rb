13b1e7b55459c653cd21752a772a462cce18d776
Matias Korhonen <matias@kiskolabs.com>
2011-01-31 10:40:58 +0200

    Actually fixes the latest Hoptoad error.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,7 @@ class ProjectsController < ApplicationController
 
   before_filter :authenticate_user!, :except => [:guest]
   before_filter :current_project, :only => [:show, :edit, :update, :destroy, :enable_guest_access, :disable_guest_access]
+  before_filter :current_organization, :only => [:new, :create]
 
   def show
     title @project.name
@@ -22,13 +23,11 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    @organization = current_organization
     title "New project for #{@organization.name}"
     @project = Project.new(:state => :ongoing)
   end
 
   def create
-    @organization = current_organization
     @project = @organization.projects.new(params[:project])
     @project.user = current_user
     flash[:notice] = "Project was successfully created." if @project.save
@@ -76,7 +75,12 @@ class ProjectsController < ApplicationController
   end
 
   def current_organization
-    current_user.organizations.find(cookies[:organization])
+    @organization = current_user.organizations.find(cookies[:organization])
+    unless @organization.present?
+      flash[:alert] = "You must choose an organization first!"
+      redirect_to organizations_path
+    end
+    
   rescue ActiveRecord::RecordNotFound
     cookies.delete(:organization)
     flash[:alert] = "You must choose an organization first!"
