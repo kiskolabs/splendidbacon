15d653c562472ff4c74342c3be9d83276c093f0b
Matias Korhonen <matias@kiskolabs.com>
2011-05-10 11:06:49 +0300

    Fixes the jQuery autolink plugin. fixes #30 (again).

--- public/javascripts/jquery.autolink.js
+++ public/javascripts/jquery.autolink.js
@@ -1,18 +1,20 @@
-jQuery.fn.autolink = function () {
-	return this.each( function(){
-	  var re, matches;
-		re = /(((http|https|ftp):\/\/|www\.)[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
-		$(this).html( $(this).html().replace(re, function(link) {
-		  var href;
-		  
-		  if ( /^www./.test(link) ) {
-		    href = 'http://' + link;
-		  }
-		  else {
-		    href = link;
-		  }
-		  
-		  return '<a href="' + href + '">' + link + '</a> ';
-		}));
-	});
-}
+(function( $ ){
+  $.fn.autolink = function() {
+    this.each( function() {
+      var re, matches;
+  		re = /(((http|https|ftp):\/\/|www\.)[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+  		$(this).html( $(this).html().replace(re, function(link) {
+  		  var href;
+
+  		  if ( /^www./.test(link) ) {
+  		    href = 'http://' + link;
+  		  }
+  		  else {
+  		    href = link;
+  		  }
+
+  		  return '<a href="' + href + '">' + link + '</a> ';
+  		}));
+    });
+  };
+})( jQuery );
