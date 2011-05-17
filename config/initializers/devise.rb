fc32b8381fd93866dab2ebf129b8df8a2e4de06e
Matias Korhonen <matias@kiskolabs.com>
2011-05-17 08:58:32 +0300

    Bump the Devise password length to 128 characters.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -84,7 +84,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 6..20.
-  # config.password_length = 6..20
+  config.password_length = 6..128
 
   # Regex to use to validate the email address
   # config.email_regexp = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
