dee2a92acce9710699960d6ac1a64d8371a71f7e
Matias Korhonen <matias@kiskolabs.com>
2012-01-09 10:18:13 +0200

    Authenticate before finding the current project.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,31 +1,31 @@
 class StatusesController < ApplicationController
-  before_filter :current_project
   before_filter :authenticate_user!
-  
+  before_filter :current_project
+
   respond_to :html
   respond_to :js, :only => [ :index ]
-  
+
   def index
     @statuses = @project.statuses.page(params[:page])
   end
-  
+
   def create
     @comment = @project.statuses.new(params[:status])
     @comment.source = "Comment"
     @comment.user = current_user
     @comment.link = nil
-    
+
     if @comment.save
       flash[:notice] = "Comment saved"
     else
       flash[:alert] = "Comment not saved"
     end
-    
+
     redirect_to project_path(@project)
   end
-  
+
   private
-  
+
   def current_project
     @project = Project.find(params[:project_id])
     raise ActiveRecord::RecordNotFound unless @project.organization.user_ids.include? current_user.id
