e84a5c8ba65ac1d4e99fc2a11c26a5b5401f78f0
Matias Korhonen <matias@kiskolabs.com>
2012-07-14 13:47:06 +0300

    Updates config files to match a new Rails 3.2 app.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-SplendidBacon::Application.config.session_store :cookie_store, :key => '_splendid_bacon_session'
+SplendidBacon::Application.config.session_store :cookie_store, key: '_splendid_bacon_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # SplendidBacon::Application.config.session_store :active_record_store
