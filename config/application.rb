dd9f967d80d4b15388f0f6d19becad8b9433ca7c
Matias Korhonen <matias@kiskolabs.com>
2012-07-14 13:01:43 +0300

    Adds precompile options for Heroku/Production.

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,10 @@ module SplendidBacon
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false
+
+    config.assets.precompile += %w( *.js *.css )
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
