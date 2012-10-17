cda5e75451581548251ba7646fe132ef8ec5acfd
Matias Korhonen <matias@kiskolabs.com>
2012-10-17 13:39:13 +0300

    SendGrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,19 @@ SplendidBacon::Application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV["HOST"] }
 
+  # Use SendGrid if configured
+  if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
+    config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+    }
+    config.action_mailer.delivery_method = :smtp
+  end
+
   # Enable threaded mode
   # config.threadsafe!
 
